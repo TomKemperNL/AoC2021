b1e419d8bf77e97d765b358dd3f1922926da410e
Tom <tomkempernl@gmail.com>
2021-12-01 20:58:14 +0100

    ...

--- AoC2021.Tests/Day1.fs
+++ AoC2021.Tests/Day1.fs
@@ -4,9 +4,33 @@ open NUnit.Framework
 open AoC2021.Day1
 
 [<SetUp>]
-let Setup () =
-    ()
+let Setup () = ()
 
 [<Test>]
-let Test1 () =
-    Assert.AreEqual(42, hello())
+let Day1APractice () =
+    let input =
+        """199
+200
+208
+210
+200
+207
+240
+269
+260
+263"""
+
+    let split = input.Split("\n") |> Array.toList
+    let ints = List.map int split
+
+    Assert.AreEqual(7, day1a ints)
+
+[<Test>]
+let Day1A () =
+    let input =
+        System.IO.File.ReadAllLines "./../../../Day1A.txt"
+
+    let split = input |> Array.toList
+    let ints = List.map int split
+
+    Assert.AreEqual(1139, day1a ints)
--- AoC2021/Day1.fs
+++ AoC2021/Day1.fs
@@ -1,5 +1,15 @@
 ﻿namespace AoC2021
 
 module Day1 =
-    let hello () =
-        42
+    let day1a (input: int list) =
+        let increase (prev: int option, total: int) cur =
+            match prev with
+            | None -> (Some cur, total)
+            | Some n ->
+                if cur > n then
+                    (Some cur, total + 1)
+                else
+                    (Some cur, total)
+
+        let (_, total) = List.fold increase (None, 0) input
+        total
