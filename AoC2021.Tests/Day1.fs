8612cfac8b768d53ab4de2101d0637d877070592
Tom <tomkempernl@gmail.com>
2021-12-01 21:16:23 +0100

    Day 1

--- AoC2021.Tests/Day1.fs
+++ AoC2021.Tests/Day1.fs
@@ -3,12 +3,7 @@ module AoC2021.Tests
 open NUnit.Framework
 open AoC2021.Day1
 
-[<SetUp>]
-let Setup () = ()
-
-[<Test>]
-let Day1APractice () =
-    let input =
+let practiceInput =
         """199
 200
 208
@@ -20,17 +15,28 @@ let Day1APractice () =
 260
 263"""
 
-    let split = input.Split("\n") |> Array.toList
-    let ints = List.map int split
+let practiceInts = practiceInput.Split("\n") |> Array.toList |> List.map int
 
-    Assert.AreEqual(7, day1a ints)
+[<SetUp>]
+let Setup () = ()
+
+[<Test>]
+let Day1APractice () =
+    Assert.AreEqual(7, day1a practiceInts)
+
+[<Test>]
+let Day1BPractice () =
+    Assert.AreEqual(5, day1b practiceInts)
+
+let input =
+        System.IO.File.ReadAllLines "./../../../Day1A.txt"
+
+let ints =  input |> Array.toList |> List.map int
 
 [<Test>]
 let Day1A () =
-    let input =
-        System.IO.File.ReadAllLines "./../../../Day1A.txt"
-
-    let split = input |> Array.toList
-    let ints = List.map int split
-
     Assert.AreEqual(1139, day1a ints)
+
+[<Test>]
+let Day1B () =
+    Assert.AreEqual(1103, day1b ints)
--- AoC2021/Day1.fs
+++ AoC2021/Day1.fs
@@ -13,3 +13,16 @@ module Day1 =
 
         let (_, total) = List.fold increase (None, 0) input
         total
+
+    let toSlidingWindows3 items = 
+        let count = List.length items
+        let itemsSkip0 = List.take (count - 2) items
+        let itemsSkip1 = List.skip 1 items |> List.take (count - 2)
+        let itemsSkip2 = List.skip 2 items |> List.take (count - 2)
+
+        List.map3 (fun x y z -> x + y + z) itemsSkip0 itemsSkip1 itemsSkip2
+
+    let day1b (input: int list) = 
+        let windowed = toSlidingWindows3 input
+        day1a windowed
+
