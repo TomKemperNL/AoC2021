59a74a835d66b52517553326a300aa00dba131a0
Tom <tom.kemper@hu.nl>
2021-12-10 13:59:48 +0100

    Dag 1 kan ietsje korter:)

--- AoC2021/Day1.fs
+++ AoC2021/Day1.fs
@@ -2,27 +2,7 @@
 
 module Day1 =
     let day1a (input: int list) =
-        let increase (prev: int option, total: int) cur =
-            match prev with
-            | None -> (Some cur, total)
-            | Some n ->
-                if cur > n then
-                    (Some cur, total + 1)
-                else
-                    (Some cur, total)
+        List.windowed 2 input |> List.filter (fun [a;b] -> b > a) |> List.length
 
-        let (_, total) = List.fold increase (None, 0) input
-        total
-
-    let toSlidingWindows3 items = 
-        let count = List.length items
-        let itemsSkip0 = List.take (count - 2) items
-        let itemsSkip1 = List.skip 1 items |> List.take (count - 2)
-        let itemsSkip2 = List.skip 2 items |> List.take (count - 2)
-
-        List.map3 (fun x y z -> x + y + z) itemsSkip0 itemsSkip1 itemsSkip2
-
-    let day1b (input: int list) = 
-        let windowed = toSlidingWindows3 input
-        day1a windowed
-
+    let day1b (input: int list) =        
+        List.windowed 3 input |> List.map List.sum |> day1a
