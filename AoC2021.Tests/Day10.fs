bffaa99468f034755079632d6340e8c8f8a74b61
Tom <tomkempernl@gmail.com>
2021-12-10 19:17:02 +0100

    Day10A

--- AoC2021.Tests/Day10.fs
+++ AoC2021.Tests/Day10.fs
@@ -2,4 +2,27 @@
 
 open System
 open NUnit.Framework
-open AoC2021.Day9
+open AoC2021.Day10
+
+let practiceInput = """[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]""".Split(Environment.NewLine) |> Array.toList
+
+[<Test>]
+let Day10APractice () =
+    Assert.AreEqual(26397, day10a practiceInput)
+    
+
+let input =
+    (System.IO.File.ReadAllLines "./../../../Day10.txt") |> Array.toList
+
+[<Test>]
+let Day10A () =
+    Assert.AreEqual(240123, day10a input)
--- AoC2021/Day10.fs
+++ AoC2021/Day10.fs
@@ -1,4 +1,72 @@
 ﻿module AoC2021.Day10
 
+let pointMap = Map.ofList [
+    (')', 3)
+    (']', 57)
+    ('}', 1197)
+    ('>', 25137)
+]
+
+type SyntaxError = {
+    expected: char
+    actual: char option
+}
+
+
+
+let parseLine (line: string) =
+    let chars = line.ToCharArray() |> Array.toList
+    
+    let rec parseLineRec (chars: char list) (brackets: char list) =
+        match chars with
+        | '(' :: rest ->
+            parseLineRec rest (')'::brackets)
+        | '[' :: rest ->
+            parseLineRec rest (']'::brackets)
+        | '{' :: rest ->
+            parseLineRec rest ('}'::brackets)
+        | '<' :: rest ->
+            parseLineRec rest ('>'::brackets)
+        | ')' :: rest ->
+            match brackets with
+            | ')'::t ->
+                parseLineRec rest t
+            | err :: t ->
+                Some { expected = ')'; actual = Some err }
+            | [] ->
+                Some { expected = ')'; actual = None }
+        | ']' :: rest ->
+            match brackets with
+            | ']'::t ->
+                parseLineRec rest t
+             | err :: t ->
+                Some { expected = ']'; actual = Some err }
+            | [] ->
+                Some { expected = ']'; actual = None }
+        | '}' :: rest ->
+            match brackets with
+            | '}'::t ->
+                parseLineRec rest t
+             | err :: t ->
+                Some { expected = '}'; actual = Some err }
+            | [] ->
+                Some { expected = '}'; actual = None }
+        | '>' :: rest ->
+            match brackets with
+            | '>'::t ->
+                parseLineRec rest t
+             | err :: t ->
+                Some { expected = '>'; actual = Some err }
+            | [] ->
+                Some { expected = '>'; actual = None }
+        | [] ->
+            None
+        | _ ->
+            failwith (sprintf "No idea what to do with %s" line)
+    parseLineRec chars []
+
 let day10a (input: string list) =
-    ()
+    let errors = List.choose parseLine input
+    let errorToInt (error: SyntaxError) =
+        Map.find error.expected pointMap
+    List.map errorToInt errors |> List.sum
