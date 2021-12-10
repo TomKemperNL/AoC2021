9713332819e48b664cdfa573d3c308a958b99112
Tom <tom.kemper@hu.nl>
2021-12-10 16:21:08 +0100

    Dag 9 pfiew

---
+++ AoC2021.Tests/Day9.fs
@@ -0,0 +1,44 @@
+﻿module AoC2021.Tests.Day9
+
+open System
+open NUnit.Framework
+open AoC2021.Day9
+
+
+let practiceInputRaw = """2199943210
+3987894921
+9856789892
+8767896789
+9899965678"""
+
+let practiceInput = practiceInputRaw.Split(Environment.NewLine) |> Array.toList
+
+[<Test>]
+let Day9APractice () =
+    Assert.AreEqual(15, day9a practiceInput)
+    
+[<Test>]    
+let GetHeight () =
+    let heightMap = parse practiceInput
+    Assert.AreEqual(2, getHeight heightMap (0,0))
+    Assert.AreEqual(9, getHeight heightMap (2,0))
+    Assert.AreEqual(9, getHeight heightMap (0,2))
+    Assert.AreEqual(7, getHeight heightMap (3,3))
+    
+
+let input =
+    (System.IO.File.ReadAllLines "./../../../Day9.txt") |> Array.toList
+        
+    
+[<Test>]
+let Day9A () =
+    Assert.AreEqual(580, day9a input)
+    
+[<Test>]
+let Day9BPractice () =
+    Assert.AreEqual(1134, day9b practiceInput)
+    
+[<Test>]
+let Day9B () =
+    let result = day9b input
+    Assert.AreEqual(856716, result)
---
+++ AoC2021/Day9.fs
@@ -0,0 +1,90 @@
+﻿module AoC2021.Day9
+
+let parse (input: string list) : int [] [] =
+    let parseLine (line: string) =
+        line.ToCharArray() |> Array.map (string >> int)
+
+    List.map parseLine input |> List.toArray
+
+let neighbours (heightmap: int [] []) (x, y) =
+    let maxX = heightmap.[0].Length - 1
+    let maxY = heightmap.Length - 1
+
+    let candidates =
+        [ (x - 1, y)
+          (x + 1, y)
+          (x, y - 1)
+          (x, y + 1) ]
+
+    let inBounds (x, y) =
+        x >= 0 && x <= maxX && y >= 0 && y <= maxY
+
+    List.filter inBounds candidates
+
+let getHeight (heightmap: int [] []) (x, y) = heightmap.[y].[x]
+
+let isLowPoint (heightmap: int [] []) (x, y) =
+    let neighbours = neighbours heightmap (x, y)
+
+    let values =
+        List.map (getHeight heightmap) neighbours
+
+    let height = getHeight heightmap (x, y)
+    List.forall ((<) height) values
+
+let lowPoints (heightmap: int [] []) =
+    seq {
+        for y in 0 .. heightmap.Length - 1 do
+            for x in 0 .. heightmap.[0].Length - 1 do
+                yield (x, y)
+    }
+    |> Seq.filter (isLowPoint heightmap)
+
+let findBasinNeighbours (heightmap: int [] []) (x, y) =
+    let height = getHeight heightmap (x, y)
+
+    let isBasinNeighbour (x, y) =
+        let neighbourHeight = getHeight heightmap (x, y)
+        neighbourHeight < 9 && neighbourHeight > height
+
+    neighbours heightmap (x, y)
+    |> List.filter isBasinNeighbour
+
+let findBasin (heightmap: int [] []) lowPoint =
+    let rec exploreBasin (edgesToExplore: Set<int * int>) (basin: Set<int * int>) =
+        match edgesToExplore with
+        | edges when (Set.count edges) = 0 -> basin
+        | edges ->
+            let newBasin = Set.union edges basin
+
+            let newEdges =
+                Set.map (fun p -> findBasinNeighbours heightmap p |> Set.ofList) edges
+                |> Set.unionMany
+            let newEdges = Set.difference newEdges basin
+
+            exploreBasin newEdges newBasin
+
+    exploreBasin (Set.singleton lowPoint) Set.empty
+
+
+
+let day9a (input: string list) =
+    let heightmap = parse input
+    let points = lowPoints heightmap
+
+    Seq.map (getHeight heightmap) points
+    |> Seq.map ((+) 1)
+    |> Seq.sum
+
+
+let day9b (input: string list) =
+    let heightmap = parse input
+    let points = lowPoints heightmap |> Seq.toList
+    let basins = List.map (findBasin heightmap) points
+
+    let sizes =
+        List.map Set.count basins
+        |> List.sortDescending
+        |> List.take 3
+
+    List.fold (*) 1 sizes
