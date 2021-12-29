5370242887f2548253dd054a0a259a8a612b558e
Tom <tomkempernl@gmail.com>
2021-12-29 19:35:12 +0100

    Hoe herschrijf ik de boom zonder mutability :S

--- AoC2021.Tests/Day18.fs
+++ AoC2021.Tests/Day18.fs
@@ -20,6 +20,17 @@ let TestToString () =
     
 
 [<Test>]
+let TestStringConversion () =
+    for case in [
+        "[5,3]"; "[[5,2],3]"; "[[1,2],[2,3]]"; "[[1,[2,4]],[[3,5],3]]"
+        "[7,[6,[5,[4,[3,2]]]]]";"[[[[0,9],2],3],4]";"[[6,[5,[4,[3,2]]]],1]";"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]";
+        "[[[[[9,8],1],2],3],4]"; "[7,[6,[5,[7,0]]]]"; "[[6,[5,[7,0]]],3]"
+        "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+        "[[3,[2,[8,0]]],[9,[5,[7,0]]]]"
+    ] do
+        Assert.AreEqual(case, case |> fromString |> toString)
+
+[<Test>]
 let TestFromString () =
     Assert.AreEqual((Raw 5, Raw 3), "[5,3]"  |> fromString)
     Assert.AreEqual((Pair (Raw 5, Raw 2), Raw 3), "[[5,2],3]" |> fromString)
@@ -29,7 +40,11 @@ let TestFromString () =
     
     
 [<Test>]
-let TestExplode () =
-    let input = Pair(Pair( Pair ( Pair(Raw 9, Raw 8), Raw 1), Raw 2), Raw 3), Raw 4
-    let expected = Pair(Pair(Pair(Raw 0, Raw 9), Raw 2), Raw 3), Raw 4
-    Assert.AreEqual(expected, reduce input) 
+let TestExplode () =    
+    Assert.AreEqual("[[[[0,9],2],3],4]", (reduce ("[[[[[9,8],1],2],3],4]" |> fromString)) |> toString)
+    Assert.AreEqual("[7,[6,[5,[7,0]]]]", (reduce ("[7,[6,[5,[4,[3,2]]]]]" |> fromString)) |> toString)    
+    Assert.AreEqual("[[6,[5,[7,0]]],3]", (reduce ("[[6,[5,[4,[3,2]]]],1]" |> fromString)) |> toString)
+    
+    Assert.AreEqual("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", (reduce ("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]" |> fromString)) |> toString)
+    Assert.AreEqual("[[3,[2,[8,0]]],[9,[5,[7,0]]]]", (reduce ("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]" |> fromString)) |> toString)
+    
--- AoC2021/Day18.fs
+++ AoC2021/Day18.fs
@@ -13,33 +13,39 @@ type SnailNumber = Value * Value
 
 let fromString (input: string) =
     let isNumeric c = Seq.contains c (seq { '0' .. '9' })
+
     let toNr chars =
-        let nrs = List.rev chars |> List.toArray |> fun ns -> new System.String(ns)
+        let nrs =
+            List.rev chars
+            |> List.toArray
+            |> fun ns -> new System.String(ns)
+
         int nrs |> Raw
 
     let rec readValue (todo: char list) (readnrs: char list) (values: Value list) =
         match todo with
         | [] ->
             match values with
-            | [result] -> result
+            | [ result ] -> result
             | _ -> failwith "Uuurgh"
-        | '[' :: rest ->
-            readValue rest readnrs values //Dit klopt niet...
+        | '[' :: rest -> readValue rest readnrs values //Dit klopt niet...
         | ']' :: rest ->
-            let values = match readnrs with
-                            | [] -> values
-                            | nrs -> toNr nrs :: values
+            let values =
+                match readnrs with
+                | [] -> values
+                | nrs -> toNr nrs :: values
+
             match values with
-            | a :: (b :: remaining) ->
-                readValue rest [] (Pair (b,a) :: remaining)
+            | a :: (b :: remaining) -> readValue rest [] (Pair(b, a) :: remaining)
             | _ -> failwith "uuuuuurgh2"
         | ',' :: rest ->
-            let values = match readnrs with
-                            | [] -> values
-                            | nrs -> toNr nrs :: values
+            let values =
+                match readnrs with
+                | [] -> values
+                | nrs -> toNr nrs :: values
+
             readValue rest [] values
-        | c :: rest when isNumeric c -> 
-            readValue rest (c :: readnrs) values
+        | c :: rest when isNumeric c -> readValue rest (c :: readnrs) values
         | _ -> failwith "wut"
 
 
@@ -63,12 +69,25 @@ let toString ((left, right): SnailNumber) =
 
 let add (left: SnailNumber) (right: SnailNumber) : SnailNumber = (Pair left, Pair right)
 
-let tryExplode (n: Value) =
+let tryExplode (n: Value) : (bool * Value)=
     let rec explodeRec nr steps =
-        match nr, steps with
-        | Raw n, _ -> false, nr
-        | Pair (left, right), 4 -> false, nr
+        match nr, steps with     
+        | Raw n, _ ->
+            false, Raw n
+        | Pair (left, right), 4 -> 
+            true, Raw 0
+        | Pair (left, right), s -> 
+            let tryLeft = explodeRec left (s+1)
+            match tryLeft with
+            | true, leftResult -> true, Pair (leftResult, right)
+            | false, leftResult ->
+                let exploded, rightResult = explodeRec right (s + 1)
+                (exploded, Pair (leftResult, rightResult))    
+    explodeRec n 0
 
-    explodeRec n 1
-
-let reduce ((left, right): SnailNumber) = 42
+let reduce ((left, right): SnailNumber) : SnailNumber =
+    match tryExplode (Pair (left, right)) with
+    | _, v ->
+        match v with
+        | Pair (left, right) -> (left,right)
+        | _ -> failwith "no resulting pair"
